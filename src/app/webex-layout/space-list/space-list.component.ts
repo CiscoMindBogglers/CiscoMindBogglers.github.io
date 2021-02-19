3ff384611856bfb0d6fcb27986c2b2289ce3113a
Sumit Kar <sumkar@cisco.com>
2021-02-19 14:39:28 +0530

    added space list

--- src/app/webex-layout/space-list/space-list.component.ts
+++ src/app/webex-layout/space-list/space-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { WebexService } from 'src/app/webex.service';
 
 @Component({
   selector: 'app-space-list',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpaceListComponent implements OnInit {
 
-  constructor() { }
-
+  displayName: string;
+  roomsList: any;
+  
+  constructor(private webex: WebexService, public router: Router) { }
   ngOnInit(): void {
+    this.webex.onInit();
+    this.webex.listRoom().then((rooms) => {
+      console.log("Printing rooms")
+      console.log(rooms.items);
+      this.roomsList = rooms.items;
+    });
   }
-
 }
