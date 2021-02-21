985f605c0824873d21a68ad296ff6c010528a186
Sumit Kar <sumkar@cisco.com>
2021-02-21 22:08:16 +0530

    adding select2 example data

--- src/app/webex-layout/broadcast/broadcast.component.ts
+++ src/app/webex-layout/broadcast/broadcast.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Select2OptionData } from 'ng-select2';
+import { Options } from 'select2';
 import { WebexService } from 'src/app/webex.service';
 
 @Component({
@@ -10,15 +12,45 @@ export class BroadcastComponent implements OnInit {
 
   constructor(private webex: WebexService) { }
   rooms: any;
-  selectedCar: number;
+  public exampleData: Array<Select2OptionData>;
+  public options: Options;
+  public _value: string[];
 
-    cars = [
-        { id: 1, name: 'Volvo' },
-        { id: 2, name: 'Saab' },
-        { id: 3, name: 'Opel' },
-        { id: 4, name: 'Audi' },
-    ];
+  get value(): string[] {
+    return this._value;
+  }
+  set value(value: string[]) {
+    console.log('Set value: ' + value);
+    this._value = value;
+  }
+
   ngOnInit(): void {
+    this.exampleData = [
+      {
+        id: 'multiple1',
+        text: 'Multiple 1'
+      },
+      {
+        id: 'multiple2',
+        text: 'Multiple 2'
+      },
+      {
+        id: 'multiple3',
+        text: 'Multiple 3'
+      },
+      {
+        id: 'multiple4',
+        text: 'Multiple 4'
+      }
+    ];
+
+    this._value = ['multiple2', 'multiple4'];
+
+    this.options = {
+      width: '300',
+      multiple: true,
+      tags: true
+    };
     this.webex.listRoom().then((rooms) => {
       console.log("Printing rooms")
       console.log(rooms.items);
