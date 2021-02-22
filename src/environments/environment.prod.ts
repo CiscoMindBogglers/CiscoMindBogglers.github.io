64f7ac8726e31c411a6f608f586508449c36bae4
Sumit Kar <sumkar@cisco.com>
2021-02-22 13:09:40 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   client_id: 'C0ef456ac130f8e0b1be37bb7c36e67bc19210f45e644c3f51c2e19c73fc02934',
-  redirect_uri: 'http://CiscoMindBogglers.github.io/webex',
+  redirect_uri: 'https://CiscoMindBogglers.github.io/webex',
   scope: 'spark:all spark:kms'
 }
