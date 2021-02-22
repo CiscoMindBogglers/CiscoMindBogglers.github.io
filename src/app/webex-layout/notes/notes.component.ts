1909a0e008102b5f0c8cdeef98d0ba8cc2639e72
Sumit Kar <sumkar@cisco.com>
2021-02-22 16:26:00 +0530

    update

--- src/app/webex-layout/notes/notes.component.ts
+++ src/app/webex-layout/notes/notes.component.ts
@@ -9,45 +9,50 @@ import { WebexService } from 'src/app/webex.service';
 export class NotesComponent implements OnInit {
 
   constructor(private webex: WebexService) { }
-
+  noteMsgs: any;
   ngOnInit(): void {
     this.webex.listRoom().then((rooms) => {
       console.log("Printing rooms")
       console.log(rooms.items);
-      var isNote=false;
+      var isNote = false;
       rooms.items.forEach(element => {
-        if(element.title==="NoteToSelf"){
+        if (element.title === "NoteToSelf") {
           localStorage.setItem(
             'note_room_id',
             element.id
           );
-          isNote=true;
+          isNote = true;
         }
       });
-      if(!isNote){
+      if (!isNote) {
         this.webex.createRoom("NoteToSelf").then((room) => {
           localStorage.setItem(
             'note_room_id',
             room.id
           );
           console.log("Notespace created");
-            })
-            .catch((error) => {
-              alert("Space not successfully created. Please contact administrator");
-              console.error(error);
-            });
-          }       
-        
-      });
+          isNote = true;
+        })
+          .catch((error) => {
+            alert("Space not successfully created. Please contact administrator");
+            console.error(error);
+          });
+      }
+
+    });
+    this.noteMsgs = this.getNoteMessages();
   }
 
-  getNoteMessages(){
-        this.webex.listMessages(localStorage.getItem('note_room_id')).then((messageDetails) => {
-          console.log("Note message details"+messageDetails);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  getNoteMessages() {
+    this.webex.listMessages(localStorage.getItem('note_room_id')).then((messageDetails) => {
+      console.log( "msgs");
+      console.log( messageDetails.items);
+      return messageDetails;
+    })
+      .catch((error) => {
+        console.error(error);
+        return {};
+      });
   }
 
 }
