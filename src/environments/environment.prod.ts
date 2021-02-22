5f4c9f966b3ae50492eeec3af221a405dd3d46a0
Sumit Kar <sumkar@cisco.com>
2021-02-22 14:23:52 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   client_id: 'C5efed444a7959c12093ba2748e4811dc90c67f4ce70b6ca2894156d9e5f679ea',
-  redirect_uri: 'https://CiscoMindBogglers.github.io/webex',
+  redirect_uri: 'https://ciscomindbogglers.web.app/webex',
   scope: 'spark:all spark:kms',
   firebase: {
     apiKey: "AIzaSyC6D6sc9MGxr-QoPW_K17dguyX40MciZ8A",
