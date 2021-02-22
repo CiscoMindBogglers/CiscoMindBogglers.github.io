4d6fc4b95d66f737d58f8211e056ff94aa11e4df
Sumit Kar <sumkar@cisco.com>
2021-02-22 14:21:58 +0530

    deploy to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,16 @@ export const environment = {
   production: false,
   client_id: 'C5efed444a7959c12093ba2748e4811dc90c67f4ce70b6ca2894156d9e5f679ea',
   redirect_uri: 'http://localhost:4200/webex',
-  scope: 'spark:all spark:kms'
+  scope: 'spark:all spark:kms',
+  firebase: {
+    apiKey: "AIzaSyC6D6sc9MGxr-QoPW_K17dguyX40MciZ8A",
+    authDomain: "ciscomindbogglers.firebaseapp.com",
+    projectId: "ciscomindbogglers",
+    storageBucket: "ciscomindbogglers.appspot.com",
+    messagingSenderId: "449691507435",
+    appId: "1:449691507435:web:911906838d2201495ebcae",
+    measurementId: "G-QJMWQH950G"
+  }
 };
 
 /*
